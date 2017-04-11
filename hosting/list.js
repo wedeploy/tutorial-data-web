a22ef6b0dbb63f1bbec514bf6c59f808e2588c4b
jonnilundy <jonathan.lundy@liferay.com>
2017-04-11 11:06:08 -0700

    remove real-time

--- hosting/list.js
+++ hosting/list.js
@@ -14,21 +14,3 @@ WeDeploy
 		});
 // Insert fetch data method above
 
-WeDeploy
-	.data('http://data.tutorial-data-js.wedeploy.io')
-	.limit(5)
-	.orderBy('id', 'desc')
-	.watch('tasks')
-	.on('changes', function(tasks) {
-		appendTasks(tasks);
-	});
-
-function appendTasks(tasks) {
-	var taskList = '';
-
-	tasks.forEach(function(task) {
-		taskList += `<input type="text" value="${task.name}" readonly>`;
-	});
-
-	list.innerHTML = taskList;
-}
