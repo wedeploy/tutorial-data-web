5f2822a9b4d8a931356e992166e12a6fa59c0117
jonnilundy <jonathan.lundy@liferay.com>
2017-04-12 17:34:19 -0700

    add appendTask function

--- hosting/list.js
+++ hosting/list.js
@@ -14,3 +14,12 @@ WeDeploy
 		});
 // Insert fetch data method above
 
+function appendTasks(tasks) {
+	var taskList = '';
+
+	tasks.forEach(function(task) {
+		taskList += `<input type="text" value="${task.name}" readonly>`;
+	});
+
+	list.innerHTML = taskList;
+}
