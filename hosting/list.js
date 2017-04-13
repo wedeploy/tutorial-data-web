ba6e8433eff0070a688c3fb0ecd2bb0935ba9592
jonnilundy <jonathan.lundy@liferay.com>
2017-04-12 17:35:09 -0700

    add appendTask function

--- hosting/list.js
+++ hosting/list.js
@@ -5,3 +5,12 @@ var list = document.querySelector('.list');
 
 // Insert fetch data method above
 
+function appendTasks(tasks) {
+	var taskList = '';
+
+	tasks.forEach(function(task) {
+		taskList += `<input type="text" value="${task.name}" readonly>`;
+	});
+
+	list.innerHTML = taskList;
+}
